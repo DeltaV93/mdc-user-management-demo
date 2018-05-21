99744a6c79ed525bde35c46a81cb7e21042c9a19
Valerie Sharp <Valeriesharp93@gmail.com>
2018-05-21 08:31:45 -0700

    moving from mdc to boostrap for demo

--- demo/toolbar.vue
+++ demo/toolbar.vue
@@ -1,20 +1,15 @@
 <template>
-  <mdc-toolbar 
-    waterfall 
+  <mdc-toolbar
+    waterfall
     class="main-toolbar">
     <mdc-toolbar-row>
       <mdc-toolbar-section align-start >
         <mdc-toolbar-menu-icon event="toggle-drawer"/>
         <mdc-toolbar-title @click="$router.push('/')">{{ 'vue\u2022mdc\u2022adapter' }}</mdc-toolbar-title>
       </mdc-toolbar-section>
-      <mdc-toolbar-section 
-        shrink-to-fit 
+      <mdc-toolbar-section
+        shrink-to-fit
         align-end>
-        <mdc-toolbar-icon 
-          href="https://github.com/stasson/vue-mdc-adapter"
-          target="_blank">
-          <img :src="githubLogo" >
-        </mdc-toolbar-icon>
       </mdc-toolbar-section>
     </mdc-toolbar-row>
   </mdc-toolbar>
