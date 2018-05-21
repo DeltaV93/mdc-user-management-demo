1282c4abd707997843b290647ba603fe30b6fb9c
Valerie Sharp <Valeriesharp93@gmail.com>
2018-05-21 00:32:53 -0700

    updating home page with user data logic

--- demo/home.vue
+++ demo/home.vue
@@ -1,27 +1,110 @@
 <template>
   <div class="demo-home">
+
+    <mdc-card>
+
+      <mdc-card-header
+        title="Title goes here"
+        subtitle="subtitle here" >
+      </mdc-card-header>
+
+      <mdc-card-actions>
+
+        <mdc-card-action-buttons>
+          <mdc-card-action-button>ACTION</mdc-card-action-button>
+        </mdc-card-action-buttons>
+
+        <mdc-card-action-icons>
+          <mdc-card-action-icon icon="star" />
+        </mdc-card-action-icons>
+
+      </mdc-card-actions>
+    </mdc-card>
+
     <mdc-display
       ref="title">{{ title }}</mdc-display>
     <about class="markdown" />
 
     <h1>{{ data.name }} <small>Parent Organization</small></h1>
-    <div v-for="location in data.locations">
-      {{ location.name }}
+    <div v-if="!data.locations && !data.subAgency">
+      <h2>This organization does not have any locations or sub agencies</h2>
+    </div>
+    <div v-else>
+      <div class="mdc-layout-grid">
+        <div class="mdc-layout-grid__inner">
+         <!--IF THEY HAVE MUTIPLE LOCATIONS-->
+          <div v-if="data.locations" class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12 demo-cell"
+               v-for="location in data.locations">{{ location.name }}<div>
+            <strong>Location Admin</strong>: {{location.admin.firstName + " " + location.admin.lastName}}
+          </div>
+            <strong>User Group:</strong>
+            <ol>
+              <li v-for="user in location.userGroup">
+                {{user.firstName + " " + user.lastName}}
+                <div>
+                  <strong>Permissions:</strong>
+                  <ul>
+                    <li v-for="permission in user.permissions">
+                      {{permission}}
+                    </li>
+                  </ul>
+                  <strong>Contact Info</strong>
+                  <ul>
+                    <li>
+                      Role: {{user.roles}}
+                    </li>
+                    <li>
+                      Email: {{user.email}}
+                    </li>
+                    <li>
+                      Phone: {{user.phone}}
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ol>
+          </div>
+          <!--IF THEY HAVE MUTIPLE LOCATIONS-->
+          <div v-if="data.subAgency">
+            <div v-for="sub in data.subAgency"
+                 class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12 demo-cell">
+                <strong>Sub Agency: </strong> {{sub.name}}
+                <!--<strong>Sub Agency Admin: </strong> {{ sub.admin.roles }}-->
+                <strong>User Group</strong>
+                <!--<strong>Admin: </strong> {{ sub.admin.email }}-->
+              <hr>
+                <!--IF SUB AGENCIES HAVE MUTIPLE LOCATIONS-->
+                <div v-if="sub.location"
+                  v-for="subLocation in sub.location">
+                  <strong>Sub Location Name: </strong> {{subLocation.name}}
+                </div>
+            </div>
+
+            </div>
+              <div v-else>
+                test
+              </div>
+            <!--</div>-->
+          </div>
+        </div>
+      </div>
+    </div>
+
     </div>
   </div>
 </template>
 
 <script>
   import userData from './mock-data/userData.json'
-export default {
-  components: {
-    about: () => import('../docs/about.md')
-  },
-  data() {
-    return {
-      title: 'Material Components for Vue.js',
-      data: userData.orgData
+  export default {
+    components: {
+      about: () => import('../docs/about.md')
+    },
+    data() {
+      return {
+        title: 'User Dropdown',
+        data: userData.orgData
+      }
     }
   }
-}
 </script>
