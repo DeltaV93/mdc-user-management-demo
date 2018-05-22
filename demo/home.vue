4f1734203360684bd086002fb7be46dd48c41079
Valerie Sharp <Valeriesharp93@gmail.com>
2018-05-21 22:17:34 -0700

    added data table. need to create user data.json

--- demo/home.vue
+++ demo/home.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="demo-home">
     <div class="col-12 col-md-9">
-
-
       <div class="row">
-
-    </div><!--/span-->
+        <!--USER TABLE-->
+        <table id="example" class="table table-striped table-bordered" style="width:100%">
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Extn.</th>
+            <th>Start date</th>
+            <th>Salary</th>
+          </tr>
+          </thead>
+          <tfoot>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Extn.</th>
+            <th>Start date</th>
+            <th>Salary</th>
+          </tr>
+          </tfoot>
+        </table>
+      </div><!--/span-->
     </div>
   </div>
 </template>
 
 <script>
   import userData from './mock-data/userData.json'
+  import './utils/tableData'
   export default {
     components: {
       about: () => import('../docs/about.md')
@@ -19,8 +40,19 @@
     data() {
       return {
         title: 'User Dropdown',
-        data: userData.orgData
+        data: userData.orgData,
+        fields: [
+          [ "Tiger Nixon", "System Architect", "$3,120", "2011/04/25", "Edinburgh", 5421 ],
+          [ "Garrett Winters", "Director", "$8,422", "2011/07/25", "Edinburgh", 8422 ],
+          // ...
+        ]
       }
+    },
+    mounted(){
+      $("#example").dataTable({
+        "data": this.fields
+      });
+
     }
   }
 </script>
